e261296bbf83bc84d117abb5b17be65f5d104c6e
Rafael Karim <refaldy.rizky22@gmail.com>
2021-08-02 11:05:56 +0700

    add endpoint register api

---
+++ handler/user.go
@@ -0,0 +1,45 @@
+package handler
+
+import (
+	"net/http"
+	"projek_go/helper"
+	"projek_go/user"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Register Account Failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	newUser, err := h.userService.RegisterUser(input)
+	if err != nil {
+		response := helper.APIResponse("Register Account Failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(newUser, "token7tq8t8o0q9t8f7D^d67O96F")
+
+	response := helper.APIResponse("Account Has Been Registered", http.StatusOK, "success", formatter)
+
+	c.JSON(http.StatusOK, response)
+}
